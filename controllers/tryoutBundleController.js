7ec0ca232d88b1b06f1c0d1076e620315e715bc3
KenzieFu <kenziefubrianto@gmail.com>
2024-05-31 13:30:33 +0700

    fix detail bundles

--- controllers/tryoutBundleController.js
+++ controllers/tryoutBundleController.js
@@ -73,31 +73,31 @@ exports.fetchOneTryoutBundle = async (req, res, next) => {
     (SELECT GROUP_CONCAT(ttb.tryout_id SEPARATOR ",") FROM tryoutbundle_tryout ttb WHERE ttb.tryoutBundle_id=tb.tryoutBundle_id) as listTryout_id,
     (SELECT GROUP_CONCAT(ut.tryout_id SEPARATOR ",") FROM usertryout ut WHERE ut.tryout_id IN (SELECT ttb.tryout_id FROM tryoutbundle_tryout ttb WHERE ttb.tryoutBundle_id=tb.tryoutBundle_id) AND ut.account_id=${account_id}) as userBought,
     (SELECT SUM(t.tryout_price) FROM tryout t WHERE t.tryout_id IN (SELECT ttb.tryout_id FROM tryoutbundle_tryout ttb WHERE ttb.tryoutBundle_id=tb.tryoutBundle_id)) as base_price
-FROM tryout_bundle tb WHERE tb.tryoutBundle_id = ${tryoutBundle_id};`,
+FROM tryout_bundle tb WHERE tb.tryoutBundle_id = ${tryoutBundle_id} LIMIT 1 ;`,
       {
         type: QueryTypes.SELECT,
       }
     );
 
-    const newBundle = bundles.map((data)=>{
-      const bundletTryout = data.listTryout_id.split(",");
-      const userClearedTryout = data.userBought?data.userBought.split(","):null;
+ 
+      const bundletTryout = bundles.listTryout_id.split(",");
+      const userClearedTryout = bundles.userBought?bundles.userBought.split(","):null;
 
-      const updatedPrice = Math.floor(data.tryout_price/bundletTryout.length) *(userClearedTryout?userClearedTryout.length:0);
+      const updatedPrice = Math.floor(bundles.tryout_price/bundletTryout.length) *(userClearedTryout?userClearedTryout.length:0);
 
-      const boolBought =data.userBought == data.listTryout_id;
-      const changedPrice= data.userBought ===null?data.tryout_price:data.tryout_price-updatedPrice;
+      const boolBought =bundles.userBought == bundles.listTryout_id;
+      const changedPrice= bundles.userBought ===null?bundles.tryout_price:bundles.tryout_price-updatedPrice;
 
-      return {
-        ...data,
+      const newData= {
+        ...bundles,
         tryout_price:changedPrice,
         listTryout_id:bundletTryout,
         userBought:userClearedTryout,
         boolBought:boolBought
       }
-    })
+    
     return res.status(200).json({
-      data: newBundle,
+      data: newData,
     });
   } catch (err) {
     next(err);
