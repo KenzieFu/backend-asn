e6f35e6890428d6544bece95d02782043aeeaaff
Machmul Pratama <mahmulpratama16@gmail.com>
2024-06-06 23:22:35 +0700

    Update for index.js data relationship

--- index.js
+++ index.js
@@ -100,6 +100,51 @@ sequelize.authenticate()
   .then(() => {
     console.log("Connection has established successfully");
     app.listen(8080, () => {
+
+      //Relationship between category and sub category
+Category.hasMany(SubCategory,{foreignKey:"category_id"})
+SubCategory.belongsTo(Category,{foreignKey:"category_id"})
+
+//Relationship between course and subcategory
+Course.belongsTo(Category,{foreignKey:"category_id"});
+Category.hasMany(Course,{foreignKey:"category_id"});
+
+//Relationship Tryout vs TryoutScore
+Tryout.hasMany(TryoutScore,{foreignKey:"tryout_id"});
+TryoutScore.belongsTo(Tryout,{foreignKey:"tryout_id"});
+
+//relationshop TryoutScore vs Account
+TryoutScore.belongsTo(Account,{foreignKey:"account_id"})
+Account.hasMany(TryoutScore,{foreignKey:"account_id"})
+
+//RelationShip Transaction vs Tryout
+UserTryout.belongsTo(Tryout,{foreignKey:"tryout_id"})
+Tryout.hasMany(UserTryout,{foreignKey:"tryout_id"});
+
+//Relationship Transaction vs Account
+UserTryout.belongsTo(Account,{foreignKey:"account_id"});
+Account.hasMany(UserTryout,{foreignKey:"account_id"})
+
+//Relationship CompleteCourse v Account 
+ClearedCourse.belongsTo(Account,{foreignKey:"account_id"})
+Account.hasMany(ClearedCourse,{foreignKey:"account_id"});
+
+//Relationship CompleteCourse v Course
+ClearedCourse.belongsTo(Course,{foreignKey:"course_id"});
+Course.hasMany(ClearedCourse,{foreignKey:"course_id"})
+
+//SKD analysis
+SKDAnalysis.belongsTo(SubCategory,{foreignKey:"subCategory_id"});
+SubCategory.hasMany(SKDAnalysis,{foreignKey:"subCategory_id"});
+
+//SKD anlaysis
+SKDAnalysis.belongsTo(Tryout,{foreignKey:"tryout_id"});
+
+//tryoutBundle and Tryout
+TryoutBundle.belongsToMany(Tryout,{through:"tryoutBundle_tryout"});
+// UserTransaction.sync({force:true})
+// Notifikasi.sync({force:true})
+
       console.log("backend-asn listening on port 8080");
     });
   })
