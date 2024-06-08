fe563ecc718c154e7d1133bea29f8de59bae2535
KenzieFu <kenziefubrianto@gmail.com>
2024-06-08 14:34:37 +0700

    update timezone

--- database/database.js
+++ database/database.js
@@ -1,9 +1,13 @@
 const Sequelize = require("@sequelize/core");
 
-const sequelize = new Sequelize("backend-asn","root","",{timezone:"+07:00"},{
+const sequelize = new Sequelize("backend-asn","root","",{
   host:"localhost",
   port:3306,
   dialect:"mysql",
+  dialectOptions: {
+    useUTC: false, 
+  },
+  timezone: '+07:00', 
   sync:true
 })
 
