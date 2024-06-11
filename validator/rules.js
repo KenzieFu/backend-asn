d9304b8ac5a9d52be542a76f10ca04181bd39732
KenzieFu <kenziefubrianto@gmail.com>
2024-06-11 23:53:28 +0700

    remove /  edit validator for update acc

--- validator/rules.js
+++ validator/rules.js
@@ -23,13 +23,6 @@ exports.updateAccValidator = [
   body('name', "Nama tidak boleh kosong").not().isEmpty(),
   body('email',"Email tidak boleh kosong").not().isEmpty(),
   body('email',"Email tidak valid").isEmail(),
-  body('password',"Password minimal memiliki 8 karakter").isLength({min:8}),
-  body('password',"Password minimal memiliki 8 karakter").custom((value)=>{
-    if (!/[A-Z]/.test(value)) {
-      throw new Error('Password harus setidaknya memiliki satu huruf kapital');
-    }
-    return true;
-  }),
   body('phone',"Nomor Handphone tidak boleh kosong").not().isEmpty(),
   body('phone',"Nomor Handphone tidak valid").matches(/^(0|62)?\d{8,13}$/)
 ]
