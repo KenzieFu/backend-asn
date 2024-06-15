2d4858edae4bf8c89083d607f335b22a85380197
KenzieFu <kenziefubrianto@gmail.com>
2024-06-15 21:03:00 +0700

    update latsol

--- database/database.js
+++ database/database.js
@@ -2,10 +2,10 @@ const Sequelize = require("@sequelize/core");
 require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.GOOGLE_CLOUD_DATABASE,
-  "root"??process.env.GOOGLE_CLOUD_SQL_USER,
- ""??process.env.GOOGLE_CLOUD_PASS,
+  process.env.GOOGLE_CLOUD_SQL_USER,
+ process.env.GOOGLE_CLOUD_PASS,
   {
-    host:"localhost"??process.env.GOOGLE_CLOUD_HOST,
+    host:process.env.GOOGLE_CLOUD_HOST,
     port: 3306,
     dialect: "mysql",
     dialectOptions: {
